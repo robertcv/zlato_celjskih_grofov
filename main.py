34da6c6993489dec24d916041eb09d0043f2f06b
Robert Cvitkovič <robert.cvitkovic@gmail.com>
2023-09-20 17:55:52 +0200

    Add refresh button

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.network.urlrequest import UrlRequest
 
 URL = "https://www.nalozbenozlato.com/si/api/get_gold_price"
@@ -10,14 +11,21 @@ URL = "https://www.nalozbenozlato.com/si/api/get_gold_price"
 
 class ZlatoCeljskihGrofovApp(App):
     def build(self):
-        self.layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='Data will be displayed here')
+        self.layout = BoxLayout(orientation='vertical', spacing=10)
+
+        self.label = Label(text='Data will be displayed here', font_size=20,  size_hint=(1, 0.7))
         self.layout.add_widget(self.label)
+
+        self.refresh_btn = Button(text='Refresh', font_size=14,  size_hint=(1, .3), on_press=self.refresh_data)
+        self.layout.add_widget(self.refresh_btn)
         return self.layout
 
-    def on_start(self):
+    def refresh_data(self, instance):
         # Make an HTTP GET request to the API
         UrlRequest(URL, on_success=self.on_success, on_error=self.on_error)
+
+    def on_start(self):
+        self.refresh_data(None)
 
     def on_success(self, urlrequest, result):
         # Handle the API response here
